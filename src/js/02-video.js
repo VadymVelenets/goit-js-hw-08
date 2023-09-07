3b7049d79e35337cecc6842a8d03818fe209bd6d
VadymVelenets <128249544+VadymVelenets@users.noreply.github.com>
2023-09-07 15:22:35 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,10 @@
-
-
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
-
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
-});
-
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const VCT_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const onPlay = function (event) {
+  localStorage.setItem(VCT_KEY, event.seconds);
 };
-
-player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
-
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY)) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,28 @@
-
-const LOCAL_KEY = 'feedback-form-state';
-
-form = document.querySelector('.feedback-form');
-
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
-
-let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 const { email, message } = form.elements;
 reloadPage();
-
-function onInputData(e) {
+function onInputData(event) {
   dataForm = { email: email.value, message: message.value };
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
-
 function reloadPage() {
   if (dataForm) {
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
   }
 }
-
-function onFormSubmit(e) {
-  e.preventDefault();
+function onFormSubmit(event) {
+  event.preventDefault();
   console.log({ email: email.value, message: message.value });
-
   if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
+    return alert(`Будь ласка, заповніть всі обов'язкові поля.`);
   }
-
-  localStorage.removeItem(LOCAL_KEY);
-  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
   dataForm = {};
 }
